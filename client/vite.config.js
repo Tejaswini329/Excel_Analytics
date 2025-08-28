575e2a0749a01f2ad95ab7cd90a50ab31b131775
Tejaswini26 <tejaswinikolapkar25@gmail.com>
2025-08-28 20:33:44 +0530

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'https://web-development-project-gxnx.onrender.com',
-    },
-  },
-});
+  base: '/' // If deployed at root, keep '/'
+})
